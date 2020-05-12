07f6d5c1ed6672edaa2167434e45fecf809bb08a
nzhyrkova <neyia@mail.ru>
2020-05-12 12:56:21 +0300

    localstorage high score

--- end.js
+++ end.js
@@ -3,6 +3,9 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores'));
+const MAX_HIGH_SCORE = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -12,5 +15,19 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (event) => {
     event.preventDefault();
-    console.log(event);
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+
+    highScores.push(score);
+
+    highScores.sort((a, b) => b.score - a.score);
+
+    highScores.splice(MAX_HIGH_SCORE);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('./index.html');
+
+    console.log(highScores);
 };
